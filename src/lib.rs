7a29626ed46fa9c7ca2394b29ab875e89d967959
Jason Heeris <jason.heeris@gmail.com>
2022-07-31 00:42:50 +0800

    Update the build code to work with the new structure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,83 @@
 //! Test binary generation for integration tests under Cargo.
 //!
+//! > ## Note
+//! >
+//! > This crate primarily exists to work around [Cargo issue #1982][cargo-1982]. If
+//! > that has been fixed, you probably don't need this.
+//!
+//!   [cargo-1982]: https://github.com/rust-lang/cargo/issues/1982
+//!
 //! If you have integration tests for things that involve subprocess management,
 //! inter-process communication, or platform tools, you might need to write some
 //! mock binaries of your own to test against. And if you're already using Cargo
 //! to build and test, it would be nice to be able to write those test binaries
-//! in Rust, in the crate you're testing, as crate binaries themselves.
+//! in Rust, near to the crate you're testing, as cargo projects themselves.
 //!
 //! This crate provides a simple interface for invoking Cargo to build test
-//! binaries in your own crate, defined in your `Cargo.toml`. Call
-//! [`build_mock_binary("name_of_binary")`](build_mock_binary) where
-//! `"name_of_binary"` is the binary name you'd pass to Cargo eg. `cargo build
-//! --bin name_of_binary`. If you need to change profiles or features, there is
-//! [`build_mock_binary_with_opts()`].
+//! binaries organised in a separate directory under your crate.
+//!
+//! The first thing to note is that these test binaries *aren't* binaries listed
+//! in your actual project's manifest. If that's what you have and it works, you
+//! don't need this crate at all — you can just use
+//! [`CARGO_BIN_EXE_<name>`][cargo-env].
+//!
+//!   [cargo-env]: https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-crates
+//!
+//! But maybe the test binaries have to be made into separate projects because
+//! they have extra dependencies. Pick a directory name and put them in there
+//! eg. this project uses `testbins`. *This is not going to be a workspace.*
+//! Under this directory you will have each test binary as a separate Cargo
+//! project, just like any other Rust binary.
+//!
+//! The structure should look something like this:
+//!
+//! ```none
+//! ├── Cargo.toml        (this crate's manifest)
+//! ├── src
+//! │  └── lib.rs         (this crate's lib.rs)
+//! ├── testbins          (all the test binary projects are under this
+//! │  │                   directory)
+//! │  ├── test-something (one test binary)
+//! │  │  ├── Cargo.toml  (test binary manifest, name = "test-something")
+//! │  │  └── src
+//! │  │     └── main.rs  (test binary source)
+//! │  ├── test-whatever  (another test binary)
+//! │  │  ├── Cargo.toml
+//! │  │  └── src
+//! │  │     └── main.rs
+//! │   ...etc...
+//! └── tests
+//!    └── tests.rs       (tests for this crate, which want to use the test
+//!                        binaries above)
+//! ```
+//!
+//! > ## Note
+//! >
+//! > It can be useful to put an empty `[workspace]` section in the `Cargo.toml`
+//! > for these test binaries, so that Cargo knows not to [look in parent
+//! > directories][cargo-10872].
+//!
+//!   [cargo-10872]: https://github.com/rust-lang/cargo/issues/10872
+//!
+//! With this setup, you can now call [`build_test_binary("test-something",
+//! "testdir")`](build_test_binary) where:
+//!
+//! - `"test-something"` is the binary name you'd pass to Cargo in the child
+//!   project eg. `cargo build --bin test-something`; it also has to be the name
+//!   of the subdirectory this project is in
+//! - `"testdir"` is the directory relative to your real project's manifest
+//!   containing this test binary project (and maybe others)
+//!
+//! If you need to change profiles or features, or have more control over the
+//! directory structure, there is also [a builder API](TestBinary).
 //!
 //! Here's an example of how you might use this in a test, with a binary named
-//! `test_it_builds`
+//! `does-build`:
 //!
 //! ```rust
-//! # use test_binary::build_mock_binary;
-//! let test_bin_path = build_mock_binary("test_it_builds").expect("Error building test binary");
+//! # use test_binary::build_test_binary;
+//! let test_bin_path = build_test_binary("does-build", "testbins")
+//!     .expect("error building test binary");
 //! let mut test_bin_subproc = std::process::Command::new(test_bin_path)
 //!     .spawn()
 //!     .expect("Error running test binary");
@@ -29,7 +88,7 @@
 //!
 //! assert!(test_bin_subproc
 //!     .wait()
-//!     .expect("Error waiting for test binary")
+//!     .expect("error waiting for test binary")
 //!     .success());
 //! ```
 //!
@@ -44,141 +103,232 @@
 #![warn(missing_docs, missing_debug_implementations)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
-use cargo_metadata::Message;
+use cargo_metadata::{BuildFinished, Message};
 use std::ffi::OsString;
 use std::io::{BufReader, Read};
+use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 
 // For the build_mock_binary_once macro.
 pub use once_cell;
 pub use paste;
 
-/// Builds the binary crate we've prepared solely for the purposes of testing
-/// this library. This goes through cargo, so it should function identically to
-/// `cargo build --bin testbin`.
-pub fn build_mock_binary(name: &str) -> Result<OsString, TestBinaryError> {
-    build_mock_binary_with_opts(name, None, [])
+macro_rules! args {
+    ($($item:expr),* $(,)?) => {
+        vec![
+            $(::std::ffi::OsString::from($item),)+
+        ]
+    };
 }
 
-/// Same as [`build_mock_binary()`] but accepts additional arguments to specify
-/// the build profile and features. To leave the profile as the default pass
-/// `None`.
-pub fn build_mock_binary_with_opts<'a, T>(
-    name: &str,
-    profile: Option<&str>,
-    features: T,
-) -> Result<OsString, TestBinaryError>
-where
-    T: IntoIterator<Item = &'a str>,
-{
-    let cargo_path = env!("CARGO");
+macro_rules! add_arg {
+    ($args:expr, $item:expr) => {
+        $args.push(::std::ffi::OsString::from($item))
+    };
+}
 
-    let mut cargo_args = vec!["build", "--message-format=json", "-q", "--bin", name];
+pub struct TestBinary<'a> {
+    binary: &'a str,
+    manifest: &'a Path,
+    features: Vec<&'a str>,
+    profile: Option<&'a str>,
+}
 
-    if let Some(prof) = profile {
-        cargo_args.push("--profile");
-        cargo_args.push(prof);
+impl<'a> TestBinary<'a> {
+    pub fn relative_to_parent(name: &'a str, manifest: &'a Path) -> Self {
+        Self {
+            binary: name,
+            manifest,
+            features: vec![],
+            profile: None,
+        }
     }
 
-    for feature in features {
-        cargo_args.push("--features");
-        cargo_args.push(feature);
+    pub fn with_profile(&mut self, profile: &'a str) -> &mut Self {
+        self.profile = Some(profile);
+        self
     }
 
-    let mut cargo_command = Command::new(cargo_path)
-        .args(cargo_args)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()?;
+    pub fn with_feature(&mut self, feature: &'a str) -> &mut Self {
+        self.features.push(feature);
+        self
+    }
 
-    let reader = BufReader::new(
-        // The child process' stdout being None is legitimately a programming
-        // error, since we created it ourselves two lines ago.
-        cargo_command
-            .stdout
-            .take()
-            .expect("cargo subprocess output has already been claimed"),
-    );
-    let mut messages = Message::parse_stream(reader);
-    let binary_path = messages
-        .find_map(|m| match m {
-            Ok(Message::CompilerArtifact(artf)) if (artf.target.name == name) => Some(artf),
+    /// Builds the binary crate we've prepared solely for the purposes of
+    /// testing this library. This goes through cargo, so it should function
+    /// identically to `cargo build --bin testbin`.
+    pub fn build(&mut self) -> Result<OsString, TestBinaryError> {
+        fn get_cargo_env_or_panic(key: &str) -> OsString {
+            std::env::var_os(key).unwrap_or_else(|| {
+                panic!(
+                    "{} '{}' {}",
+                    "The environment variable ",
+                    key,
+                    "is not set, is this running under a 'cargo test' command?"
+                )
+            })
+        }
+
+        let cargo_path = get_cargo_env_or_panic("CARGO");
+
+        // Resolve test binary project manifest.
+        let mut manifest_path = PathBuf::from(get_cargo_env_or_panic("CARGO_MANIFEST_DIR"));
+        manifest_path.push(self.manifest);
+
+        let mut cargo_args = args![
+            "build",
+            "--message-format=json",
+            "-q",
+            "--manifest-path",
+            manifest_path,
+            "--bin",
+            self.binary,
+        ];
+
+        if let Some(prof) = self.profile {
+            add_arg!(cargo_args, "--profile");
+            add_arg!(cargo_args, prof);
+        }
+
+        for feature in &self.features {
+            add_arg!(cargo_args, "--features");
+            add_arg!(cargo_args, feature);
+        }
+
+        let mut cargo_command = Command::new(cargo_path)
+            .args(cargo_args)
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()?;
+
+        let reader = BufReader::new(
+            // The child process' stdout being None is legitimately a programming
+            // error, since we created it ourselves two lines ago.
+            cargo_command
+                .stdout
+                .take()
+                .expect("cargo subprocess output has already been claimed"),
+        );
+
+        let mut messages = Message::parse_stream(reader);
+        let cargo_outcome = messages.find_map(|m| match m {
+            Ok(Message::CompilerArtifact(artf)) if (artf.target.name == self.binary) => Some(
+                artf.executable
+                    .ok_or_else(|| TestBinaryError::BinaryNotBuilt(self.binary.to_owned())),
+            ),
+            Ok(Message::BuildFinished(BuildFinished { success: false, .. })) => {
+                Some(Err(TestBinaryError::BuildError))
+            }
             _ => None,
-        })
-        .and_then(|a| a.executable)
-        .ok_or_else(|| TestBinaryError::BinaryNotFound(name.to_owned()));
+        });
 
-    // See above re. stderr being None.
-    let mut error_reader = BufReader::new(
-        cargo_command
-            .stderr
-            .take()
-            .expect("cargo subprocess error output has already been claimed"),
-    );
+        // See above re. stderr being None.
+        let mut error_reader = BufReader::new(
+            cargo_command
+                .stderr
+                .take()
+                .expect("cargo subprocess error output has already been claimed"),
+        );
 
-    let mut error_msg = String::new();
+        let mut error_msg = String::new();
 
-    error_reader.read_to_string(&mut error_msg)?;
+        error_reader.read_to_string(&mut error_msg)?;
 
-    if cargo_command.wait()?.success() {
-        Ok(binary_path?.into())
-    } else {
-        Err(TestBinaryError::CargoFailure(error_msg))
+        if cargo_command.wait()?.success() {
+            // The process succeeded. There should be a result from the JSON
+            // output above.
+            cargo_outcome
+                .expect("Cargo succeeded but produced no output")
+                .map(Into::into)
+        } else if let Some(Err(err)) = cargo_outcome {
+            // The process failed and there's an error we extracted from the
+            // JSON output. Usually this means a compiler error.
+            Err(err)
+        } else {
+            // The process failed but there's no error from the JSON output.
+            // This will happen if there's an invocation error eg. the manifest
+            // does not exist.
+            //
+            // This case also covers process failure but an Ok() result from the
+            // above message parsing. This would be strange (if it's even
+            // possible), but if it happens we should still report the error.
+            Err(TestBinaryError::CargoFailure(error_msg))
+        }
     }
+}
+
+pub fn build_test_binary<R: AsRef<Path>>(
+    name: &str,
+    directory: R,
+) -> Result<OsString, TestBinaryError> {
+    TestBinary::relative_to_parent(
+        name,
+        &PathBuf::from_iter([directory.as_ref(), name.as_ref(), "Cargo.toml".as_ref()]),
+    )
+    .build()
 }
 
 /// Error type for build result.
 #[derive(thiserror::Error, Debug)]
 pub enum TestBinaryError {
     /// An error running cargo itself.
-    #[error("error running cargo")]
+    #[error("IO error running Cargo")]
     CargoRunError(#[from] std::io::Error),
     /// Cargo ran but did not succeed.
-    #[error("cargo failed (message is stderr only)")]
+    #[error("Cargo failed, stderr: {0}")]
     CargoFailure(String),
     /// Cargo ran but there was a compilation error.
-    #[error("build error: {0}")]
-    BuildError(String),
+    #[error("build error")]
+    BuildError,
     /// Cargo ran and seemed to succeed but the requested binary did not appear
     /// in its build output.
-    #[error(r#"could not find binary "{0}" in cargo output"#)]
-    BinaryNotFound(String),
+    #[error(r#"could not find binary "{0}" in Cargo output"#)]
+    BinaryNotBuilt(String),
 }
 
 /// Generate a singleton function to save invoking cargo multiple times for the
-/// same binary. This is useful when you have many integration tests that use
-/// the one test binary, and don't want to invoke Cargo over and over for each
-/// one.
+/// same binary.
 ///
-/// Calling `build_mock_binary_once(binary_name)` (no quotes) will generate a
-/// function `binary_name_path()` that returns the path of the built test binary
-/// as an `OsString`, just like `build_mock_binary("binary_name")` would. Unlike
-/// `build_mock_binary("binary_name")`, the generated function will only build
-/// the binary once, and only on the first call. Subsequent calls will use a
-/// cached path and assume the initial build is still valid. The generated
-/// function unwraps the result internally and will panic on build errors.
+/// This is useful when you have many integration tests that use the one test
+/// binary, and don't want to invoke Cargo over and over for each one. Note that
+/// Cargo itself implements both locking and caching at the filesystem level, so
+/// all this macro will save you is the overhead of spawning the Cargo process
+/// to do its checks. That may still be appreciable for high numbers of tests or
+/// on slow systems.
 ///
-/// For example, if you use `build_mock_binary_once(test_it_builds)` in
+/// Calling `build_test_binary_once!(binary_name, "tests_dir")` (no quotes on
+/// `binary_name`) will generate a function `path_to_binary_name()` that returns
+/// the path of the built test binary as an `OsString`, just like
+/// `build_test_binary("binary_name", "tests_dir")` would. Unlike
+/// `build_test_binary()`, the generated function will only build the binary
+/// once, and only on the first call. Subsequent calls will use a cached path
+/// and assume the initial build is still valid. The generated function unwraps
+/// the result internally and will panic on build errors.
+///
+/// For example, if you use `build_test_binary_once!(my_test, "testbins")` in
 /// `tests/common/mod.rs`, that module will then contain a function
-/// `test_it_builds_path() -> std::ffi::OsString`. Multiple integration tests
-/// can then use `common::test_it_builds_path()` to obtain the path. Cargo will
-/// only be run once for this binary, even if the integration
-/// tests that use it are being run in multiple threads.
+/// `path_to_my_test() -> std::ffi::OsString`. Multiple integration tests can
+/// then use `common::path_to_my_test()` to obtain the path. Cargo will only be
+/// run once for this binary, even if the integration tests that use it are
+/// being run in multiple threads.
 ///
 /// See this module's own integration tests for an example. If you need to use
-/// the extra arguments from [`build_mock_binary_with_opts()`] you will need to
-/// implement it manually.
+/// extra features or a non-default profile, you will need to go back to using
+/// the builder.
 #[macro_export]
-macro_rules! build_mock_binary_once {
-    ($name:ident) => {
+macro_rules! build_test_binary_once {
+    ($name:ident, $tests_dir:expr) => {
         $crate::paste::paste! {
-            pub fn [<$name _path>]() -> std::ffi::OsString {
+            pub fn [<path_to_ $name>]() -> std::ffi::OsString {
                 use $crate::once_cell::sync::Lazy;
                 use std::ffi::OsString;
 
-                static [<$name _path_lazy>]: Lazy<OsString> =
-                    Lazy::new(|| $crate::build_mock_binary(stringify!($name)).unwrap());
-                [<$name _path_lazy>].clone()
+                static [<lazy_path_to_ $name>]: Lazy<OsString> =
+                    Lazy::new(|| $crate::build_test_binary(
+                        stringify!($name),
+                        $tests_dir
+                    ).unwrap());
+                [<lazy_path_to_ $name>].clone()
             }
         }
     };
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,32 +1,40 @@
 //! Integration tests for mock binary builds.
 
-use std::path::PathBuf;
-use test_binary::{
-    build_mock_binary, build_mock_binary_once, build_mock_binary_with_opts, TestBinaryError,
-};
+use std::path::{Path, PathBuf};
+use test_binary::{build_test_binary, build_test_binary_once, TestBinary, TestBinaryError};
 
 // Singleton function for "test_multiple" binary.
-build_mock_binary_once!(test_multiple);
+build_test_binary_once!(multiple, "testbins");
+
+fn assert_path_end<R: AsRef<Path>>(actual: R, expected_ending: &str) {
+    assert!(actual.as_ref().ends_with(expected_ending))
+}
 
 /// Test that a binary which should build, does build.
 #[test]
 fn test_builds() {
-    let result = build_mock_binary("test_it_builds");
-    assert!(PathBuf::from(result.unwrap()).ends_with("test_it_builds"));
+    let result = build_test_binary("does-build", "testbins");
+    assert_path_end(result.unwrap(), "does-build");
 }
 
 /// Test building a binary with a non-default profile (release).
 #[test]
 fn test_release() {
-    let result = build_mock_binary_with_opts("test_it_builds", Some("release"), []);
-    assert!(PathBuf::from(result.unwrap()).ends_with("test_it_builds"));
+    let result = TestBinary::relative_to_parent(
+        "does-build",
+        &PathBuf::from_iter(["testbins", "does-build", "Cargo.toml"]),
+    )
+    .with_profile("release")
+    .build();
+
+    assert_path_end(result.unwrap(), "does-build");
 }
 
 /// Test that building a binary that doesn't build produces an error.
 #[test]
 fn test_doesnt_build() {
-    let result = build_mock_binary("test_doesnt_build");
-    assert!(matches!(result, Err(TestBinaryError::BuildError(_))));
+    let result = build_test_binary("doesnt-build", "testbins");
+    assert!(matches!(result, Err(TestBinaryError::BuildError)));
 }
 
 /// Test that building a binary that doesn't exist produces an error. Note that
@@ -35,7 +43,7 @@ fn test_doesnt_build() {
 /// prose on stderr.
 #[test]
 fn test_doesnt_exist() {
-    let result = build_mock_binary_with_opts("test_doesnt_exist", None, []);
+    let result = build_test_binary("doesnt-exist", "testbins");
     assert!(matches!(result, Err(TestBinaryError::CargoFailure(_))));
 }
 
@@ -44,24 +52,24 @@ fn test_doesnt_exist() {
 /// or timing, but they act as a check against the macro failing to do its job.
 #[test]
 fn test_multiple_calls_1() {
-    let result = test_multiple_path();
-    assert!(PathBuf::from(result).ends_with("test_multiple"));
+    let result = path_to_multiple();
+    assert_path_end(result, "multiple");
 }
 
 #[test]
 fn test_multiple_calls_2() {
-    let result = test_multiple_path();
-    assert!(PathBuf::from(result).ends_with("test_multiple"));
+    let result = path_to_multiple();
+    assert_path_end(result, "multiple");
 }
 
 #[test]
 fn test_multiple_calls_3() {
-    let result = test_multiple_path();
-    assert!(PathBuf::from(result).ends_with("test_multiple"));
+    let result = path_to_multiple();
+    assert_path_end(result, "multiple");
 }
 
 #[test]
 fn test_multiple_calls_4() {
-    let result = test_multiple_path();
-    assert!(PathBuf::from(result).ends_with("test_multiple"));
+    let result = path_to_multiple();
+    assert_path_end(result, "multiple");
 }
